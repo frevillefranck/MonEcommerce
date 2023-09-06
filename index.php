7b4f368e5bc9d7aec8c426d49520890057dbf318
franck <freville.franck@gmail.com>
2023-09-06 11:10:11 +0200

    maj

--- index.php
+++ index.php
@@ -6,11 +6,10 @@ require('./inc/haut.inc.php');
 <h2>Notre page d'accueil</h2>
 <p>Coincée entre le haut et le bas !</p>
 <?php
+
+// echo getenv('NOM_VARIABLE');
+// echo '<br>';
+// echo $_ENV['NOM_VARIABLE'];
+
+// echo $_ENV['TEST'];
 require('./inc/bas.inc.php');
-// var_dump(RACINE_SITE);
-echo '<br>';
-// echo session_status();
-// Notre accueil
-echo '<h2>Notre futur page d\'accueil pour notre boutique</h2>';
-?>
-<img src="<?php echo RACINE_SITE; ?>inc/img/noir.jpg" alt="test image">
